9b8749963ea246ad263554529d20140c1735eda7
goranterzic <goranterzicunity@gmail.com>
2021-03-02 14:26:56 +0100

    Initial commit

---
+++ Assets/Scripts/Bigboss.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bigboss : MonoBehaviour
+{
+    # region Fields
+    bool   changeDirection = false;
+    int    randomColor;
+    float  randomDirection;
+    float  randomSpeedDirection;
+
+    public GameObject Poop;
+    GameObject PoopInstante;
+    #endregion
+
+    #region Methods
+    void Start()
+    {
+        InvokeRepeating("ColorSet", 0.17f, 0.37f);   
+    }
+    void FixedUpdate()
+    {  
+        ChangeDirection();   
+    }
+    void ColorSet()
+    {
+        randomColor = Random.Range(1, 5);
+        PoopInstante = Instantiate(Poop, new Vector3(transform.position.x, transform.position.y - 0.3f), Quaternion.identity);
+        PoopInstante.name = "PoopInstance";
+        SpeedSet(PoopInstante,ChangeDirection());
+        Renderer PoopRenderer = PoopInstante.GetComponent<Renderer>();
+        switch (randomColor)
+        {
+            case 1:
+                PoopRenderer.material.SetColor("_Color", Color.red);
+
+                break;
+            case 2:
+                PoopRenderer.material.SetColor("_Color", Color.magenta);
+
+                break;
+            case 3:
+                PoopRenderer.material.SetColor("_Color", Color.yellow);
+
+                break;
+            case 4:
+                PoopRenderer.material.SetColor("_Color", Color.green);
+
+                break;
+            case 5:
+                PoopRenderer.material.SetColor("_Color", Color.blue);
+
+                break;
+            default:
+                Debug.Log("Greska iz nekog razloga. ");
+                break;
+        }
+        Destroy(PoopInstante, 1f);
+    }
+    Vector2 SpeedSet(GameObject Poop, float randomSpeedDirection)
+    {
+      Vector2 PoopSpeed = Poop.GetComponent<Rigidbody>().velocity= new Vector2(0,-1  * randomSpeedDirection);
+      return  PoopSpeed;
+    }
+    float ChangeDirection() 
+    {
+        randomSpeedDirection = Random.Range(4, 11);
+        randomDirection = Random.Range(0.2f, 1.58f);
+        if (changeDirection)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * randomSpeedDirection);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * randomSpeedDirection);
+        }
+        if (transform.position.x >= randomDirection)
+        {
+            changeDirection = false;
+        }
+        if (transform.position.x <= -randomDirection)
+        {
+            changeDirection = true;
+        }
+        return randomSpeedDirection;
+    }
+    #endregion
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    # region Fields
+    public GameObject scoree;
+    public GameObject LawyerInstance;
+   
+    Text rezultatUI;
+    
+    int score;
+    #endregion
+    #region Methods
+    private void Awake()
+    {
+        rezultatUI = scoree.GetComponent<Text>();
+    }
+    void FixedUpdate()
+    {
+        rezultatUI.text = "Score: " + score;
+        PlayerMouvment();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {    
+        if (collision.gameObject.GetComponent<Renderer>().material.color.Equals(Color.red))
+        {
+            score += 500;
+        }
+        else if (collision.gameObject.GetComponent<Renderer>().material.color.Equals(Color.magenta))
+        {
+            score -= 250;
+        }
+        else if (collision.gameObject.name == "managerLeft")
+        {
+            score -= 2000;
+        }
+        else if (collision.gameObject.name == "managerRight")
+        {
+            score -= 2000;
+        }
+        Destroy(collision.gameObject);
+    }
+    void PlayerMouvment() 
+    { 
+        transform.position = new Vector2(Mathf.Clamp(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, -2.4f,2.4f),
+        Mathf.Clamp(Camera.main.ScreenToWorldPoint(Input.mousePosition).y,-4.3f,0.5f));   
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Lawyers")
+        {
+            score += 123;
+            Destroy(other.gameObject);
+        
+        } 
+    }
+    #endregion
+}
